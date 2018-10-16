7f2c691a9acdc8f6327920da8f01e880d882038a
José Simões <josesimoes@users.noreply.github.com>
2018-10-16 16:21:14 +0100

    Update commit-assemblyinfo-changes.ps1

--- commit-assemblyinfo-changes.ps1
+++ commit-assemblyinfo-changes.ps1
@@ -7,7 +7,7 @@ else
 {
     # updated assembly info files   
     git add "source\nanoFramework.System.Net\Properties\AssemblyInfo.cs"
-    git commit -m "Update assembly info file for v$env:GitVersion_NuGetVersionV2"
+    git commit -m "Update assembly info file for v$env:GitVersion_NuGetVersionV2 [skip ci]" -m"[version update]"
     git push origin --porcelain -q > $null
     
     'Updated assembly info...' | Write-Host -ForegroundColor White -NoNewline
