325476f2937b4771da8bd7e8d68be2e9f2b9d0ee
José Simões <jose.simoes@eclo.solutions>
2021-03-18 09:23:21 +0000

    Improvements in Network information
    - Add GetIsNetworkAvailable to NetworkInformation.
    - Add IPGlobalProperties class implementing GetIPAddress.
    - Add sonarlint rules to solution.
    - Add several exclude rules to project file.
    - Bump version to 1.6.4-preview.
    - Bump AssemblyNativeVersion to 100.1.3.2.

--- nanoFramework.System.Net/Properties/AssemblyInfo.cs
+++ nanoFramework.System.Net/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
-[assembly: AssemblyNativeVersion("100.1.3.1")]
+[assembly: AssemblyNativeVersion("100.1.3.2")]
 ////////////////////////////////////////////////////////////////
 
 // Setting ComVisible to false makes the types in this assembly not visible 
