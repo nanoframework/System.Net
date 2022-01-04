98a0c07b39be51bf817e3b8ab7cac532e51afd66
José Simões <jose.simoes@eclo.solutions>
2022-01-04 16:02:17 +0000

    Various fixes and improvements
    - Add missing Equals and GetHashCode on several classes.
    - Rework GetHostEntry to properly parse data from native call.
    - Apply back fix from #217
    - Fix several comments for documentation.
    - Several fixes in code style.
    - Improvements from analysers.
    - Disable running Unit Tests.

--- nanoFramework.System.Net/IPAddress.cs
+++ nanoFramework.System.Net/IPAddress.cs
@@ -11,29 +11,28 @@ using System.Runtime.CompilerServices;
 namespace System.Net
 {
     /// <summary>
-    /// Provides an internet protocol (IP) address.
+    /// Initializes a new instance of the <see cref="IPAddress"/> class.
     /// </summary>
     [Serializable]
     public class IPAddress
     {
         /// <summary>
-        /// Provides an IP address that indicates that the server must listen for client activity on all network interfaces. 
-        /// This field is read-only.
+        /// Provides an IP address that indicates that the server must listen for client activity on all network interfaces. This field is read-only.
         /// </summary>
-        public static readonly IPAddress Any = new IPAddress(0x0000000000000000);
-        
+        public static readonly IPAddress Any = new(0x0000000000000000);
+
         /// <summary>
         /// Provides the IP loopback address. This field is read-only.
         /// </summary>
-        public static readonly IPAddress Loopback = new IPAddress(0x000000000100007F);
+        public static readonly IPAddress Loopback = new(0x000000000100007F);
 
-        internal long Address;
-
-        [Diagnostics.DebuggerBrowsable(Diagnostics.DebuggerBrowsableState.Never)]
-        private AddressFamily _family = AddressFamily.InterNetwork;
+        internal readonly long Address;
 
         [Diagnostics.DebuggerBrowsable(Diagnostics.DebuggerBrowsableState.Never)]
-        private ushort[] _numbers = new ushort[NumberOfLabels];
+        private readonly AddressFamily _family = AddressFamily.InterNetwork;
+
+        [Diagnostics.DebuggerBrowsable(Diagnostics.DebuggerBrowsableState.Never)]
+        private readonly ushort[] _numbers = new ushort[NumberOfLabels];
 
         internal const int IPv4AddressBytes = 4;
         internal const int IPv6AddressBytes = 16;
@@ -55,12 +54,22 @@ namespace System.Net
         /// <summary>
         /// Initializes a new instance of the <see cref="IPAddress"/> class with the address specified as an Int64.
         /// </summary>
-        /// <param name="newAddress"></param>
+        /// <param name="newAddress">The long value of the IP address. For example, the value 0x2414188f in big-endian format would be the IP address "143.24.20.36".</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// <paramref name="newAddress"/> &lt; 0 or
+        /// <paramref name="newAddress"/> &gt; 0x00000000FFFFFFFF
+        /// </exception>
+        /// <remarks>
+        /// The IPAddress instance is created with the <see cref="Address"/> property set to <paramref name="newAddress"/>.
+        /// The <see cref="Int64"/> value is assumed to be in network byte order.
+        /// </remarks>
         public IPAddress(long newAddress)
         {
             if (newAddress < 0 || newAddress > 0x00000000FFFFFFFF)
             {
+#pragma warning disable S3928 // Parameter names used into ArgumentException constructors should match an existing one 
                 throw new ArgumentOutOfRangeException();
+#pragma warning restore S3928 // OK to throw this here
             }
 
             Address = newAddress;
@@ -73,15 +82,29 @@ namespace System.Net
         /// Initializes a new instance of the <see cref="IPAddress"/> class with the address specified as a Byte array.
         /// </summary>
         /// <param name="address"></param>
+        /// <exception cref="ArgumentNullException"><paramref name="address"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="address"/> contains a bad IP address.</exception>
+        /// <remarks>
+        /// The IPAddress is created with the <see cref="Address"/> property set to <paramref name="address"/>.
+        /// If the length of <paramref name="address"/> is 4, <see cref="IPAddress"/>(Byte[]) constructs an IPv4 address; otherwise, an IPv6 address with a scope of 0 is constructed.
+        /// The <see cref="Byte"/> array is assumed to be in network byte order with the most significant byte first in index position 0.
+        /// </remarks>
         public IPAddress(byte[] address)
         {
-            if (address[0] == (byte)AddressFamily.InterNetwork)
+            if (address == null)
+            {
+#pragma warning disable S3928 // Parameter names used into ArgumentException constructors should match an existing one 
+                throw new ArgumentNullException();
+#pragma warning restore S3928 // OK to throw this here
+            }
+
+            if (address.Length == IPv4AddressBytes)
             {
                 _family = AddressFamily.InterNetwork;
-                // need to offset address by 4 (1st are family, 2nd are port
-                Address = ((address[3 + 4] << 24 | address[2 + 4] << 16 | address[1 + 4] << 8 | address[0 + 4]) & 0x0FFFFFFFF);
+
+                Address = (address[3] << 24 | address[2] << 16 | address[1] << 8 | address[0]) & 0x0FFFFFFFF;
             }
-            else if (address[0] == (byte)AddressFamily.InterNetworkV6)
+            else if (address.Length == IPv6AddressBytes)
             {
                 _family = AddressFamily.InterNetworkV6;
 
@@ -93,7 +116,9 @@ namespace System.Net
             else
             {
                 // unsupported address family
-                throw new NotSupportedException();
+#pragma warning disable S3928 // Parameter names used into ArgumentException constructors should match an existing one 
+                throw new ArgumentException();
+#pragma warning restore S3928 // OK to throw this here
             }
         }
 
@@ -140,12 +165,10 @@ namespace System.Net
         /// <summary>
         /// Converts an Internet address to its standard notation.
         /// </summary>
-        /// <returns>A string that contains the IP address in either IPv4 dotted-quad or 
-        /// in IPv6 colon-hexadecimal notation.
+        /// <returns>A string that contains the IP address in either IPv4 dotted-quad or in IPv6 colon-hexadecimal notation.
         /// </returns>
         /// <remarks>
-        /// The ToString method converts the IP address that is stored in the Address property to either IPv4 dotted-quad or 
-        /// IPv6 colon-hexadecimal notation.
+        /// The <see cref="ToString"/> method converts the IP address that is stored in the <see cref="Address"/> property to either IPv4 dotted-quad or IPv6 colon-hexadecimal notation.
         /// </remarks>
         public override string ToString()
         {
@@ -161,6 +184,7 @@ namespace System.Net
             NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
 
             int cnt = interfaces.Length;
+
             for (int i = 0; i < cnt; i++)
             {
                 NetworkInterface ni = interfaces[i];
@@ -180,6 +204,23 @@ namespace System.Net
             return Any;
         }
 
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            // For IPv6 addresses, we cannot simply return the integer
+            // representation as the hashcode. Instead, we calculate
+            // the hashcode from the string representation of the address.
+            if (_family == AddressFamily.InterNetworkV6)
+            {
+                return ToString().GetHashCode();
+            }
+            else
+            {
+                // For IPv4 addresses, we can simply use the integer representation.
+                return unchecked((int)Address);
+            }
+        }
+
         // For security, we need to be able to take an IPAddress and make a copy that's immutable and not derived.
         internal IPAddress Snapshot()
         {
@@ -188,8 +229,8 @@ namespace System.Net
                 case AddressFamily.InterNetwork:
                     return new IPAddress(Address);
 
-                //case AddressFamily.InterNetworkV6:
-                //    return new IPAddress(m_Numbers, (uint)m_ScopeId);
+                    //case AddressFamily.InterNetworkV6:
+                    //    return new IPAddress(m_Numbers, (uint)m_ScopeId);
             }
 
             throw new NotSupportedException();
